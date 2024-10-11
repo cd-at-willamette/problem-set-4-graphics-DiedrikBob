f83c44933a41a9ae85127e6e5d88c23f824228f6
DiedrikBob <dcboberg@willamette.edu>
2024-10-10 21:37:18 -0700

    Update Prob2.py
    took me longer than I thought it would

--- Prob2.py
+++ Prob2.py
@@ -1,7 +1,7 @@
 ########################################
-# Name:
-# Collaborators:
-# Estimated time spent (hrs):
+# Name: Diedrik Boberg
+# Collaborators:- 
+# Estimated time spent (hrs): 2
 ########################################
 
 from pgl import GWindow, GRect
@@ -10,20 +10,43 @@ WIDTH = 800
 HEIGHT = 600
 BRICK_WIDTH = 40
 BRICK_HEIGHT = 20
-BRICKS_IN_BASE = 15
+BRICKS_IN_BASE = 12
 
 def draw_pyramid():
     """ 
     Draws a pyramid of bricks centered on the screen with a height of BRICKS_IN_BASE. 
     """
-
     gw = GWindow(WIDTH, HEIGHT)
 
-    # You got it from here
+    def my_red_rect(x,y,w,h): #red brick that I'll be placing in the middle of the window to confirm the location of the pyramid
+        rect = GRect(x,y,w,h)
+        rect.set_filled(True)
+        rect.set_fill_color("red")
+        gw.add(rect)
+
+    def my_rect(x,y,w,h): #function to draw a brick
+        rect = GRect(x,y,w,h)
+        rect.set_filled(False)
+        gw.add(rect)
+    
+    def draw_brick():
+        for y in range(BRICKS_IN_BASE):
+            for x in range(1,y+2):
+                my_rect((WIDTH/2 - BRICK_WIDTH/2) - y * BRICK_WIDTH/2 + x *BRICK_WIDTH  - BRICK_WIDTH,    (HEIGHT/2 - (BRICKS_IN_BASE/2 * BRICK_HEIGHT)) + BRICK_HEIGHT * y, BRICK_WIDTH, BRICK_HEIGHT) #I somehow got the pyramid to be placed at the right location, i don't understand the math behind it, it's wo weird with the "grid" being upside down
+          
 
 
+    draw_brick()
+    
+
+    my_red_rect(WIDTH//2 - BRICK_WIDTH//2, HEIGHT//2 - BRICK_HEIGHT//2, BRICK_WIDTH, BRICK_HEIGHT) #check where the middle is
+    
+    
+    
 
 
+    
+  
 
 
 
