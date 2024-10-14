c6d0230b124f8c29bbbcf1b7eadda5bc4239c785
DiedrikBob <dcboberg@willamette.edu>
2024-10-14 09:14:58 -0700

    Update Prob2.py
    added comments

--- Prob2.py
+++ Prob2.py
@@ -1,7 +1,7 @@
 ########################################
-# Name: Diedrik Boberg
-# Collaborators:- 
-# Estimated time spent (hrs): 2
+# Name: Diedrik
+# Collaborators: Anika
+# Estimated time spent (hrs): 3
 ########################################
 
 from pgl import GWindow, GRect
@@ -10,7 +10,7 @@ WIDTH = 800
 HEIGHT = 600
 BRICK_WIDTH = 40
 BRICK_HEIGHT = 20
-BRICKS_IN_BASE = 12
+BRICKS_IN_BASE = 15
 
 def draw_pyramid():
     """ 
@@ -18,11 +18,14 @@ def draw_pyramid():
     """
     gw = GWindow(WIDTH, HEIGHT)
 
+    """
     def my_red_rect(x,y,w,h): #red brick that I'll be placing in the middle of the window to confirm the location of the pyramid
         rect = GRect(x,y,w,h)
         rect.set_filled(True)
         rect.set_fill_color("red")
         gw.add(rect)
+
+    """
 
     def my_rect(x,y,w,h): #function to draw a brick
         rect = GRect(x,y,w,h)
@@ -30,8 +33,8 @@ def draw_pyramid():
         gw.add(rect)
     
     def draw_brick():
-        for y in range(BRICKS_IN_BASE):
-            for x in range(1,y+2):
+        for y in range(BRICKS_IN_BASE): #going down BRICKS_IN_BASE rows
+            for x in range(1,y+2): #placing number of bricks on each row
                 my_rect((WIDTH/2 - BRICK_WIDTH/2) - y * BRICK_WIDTH/2 + x *BRICK_WIDTH  - BRICK_WIDTH,    (HEIGHT/2 - (BRICKS_IN_BASE/2 * BRICK_HEIGHT)) + BRICK_HEIGHT * y, BRICK_WIDTH, BRICK_HEIGHT) #I somehow got the pyramid to be placed at the right location, i don't understand the math behind it, it's wo weird with the "grid" being upside down
           
 
@@ -39,27 +42,8 @@ def draw_pyramid():
     draw_brick()
     
 
-    my_red_rect(WIDTH//2 - BRICK_WIDTH//2, HEIGHT//2 - BRICK_HEIGHT//2, BRICK_WIDTH, BRICK_HEIGHT) #check where the middle is
+    #my_red_rect(WIDTH//2 - BRICK_WIDTH//2, HEIGHT//2 - BRICK_HEIGHT//2, BRICK_WIDTH, BRICK_HEIGHT) #check where the middle is
     
-    
-    
-
-
-    
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     draw_pyramid()
